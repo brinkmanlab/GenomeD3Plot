c73faa98bb57bad7459da3db780296c2cdf71b9e
adrianclim <aclim@sfu.ca>
2016-01-18 12:38:22 -0800

    Initial commit of Genome class, added setAlignmentFile method

--- comparativeplot.js
+++ comparativeplot.js
@@ -1,15 +1,26 @@
 function ComparativePlot(targetContainer){
-    this.sequences = [];
+    this.genomes = [];
+    this.alignmentFile = null;
     this.container = d3.select(targetContainer);
     this.svg = null;
 }
+
+ComparativePlot.prototype.addGenome = function(genome){
+    this.genomes.push(genome);
+};
+
+ComparativePlot.prototype.setAlignmentFile = function(alignmentfile){
+    this.alignmentFile = alignmentfile;
+};
 
 ComparativePlot.prototype.render = function(){
     this.svg = this.container.append("svg")
         .attr("class","comparativeplot");
 };
 
-
+function Genome(name){
+    this.genomeName = name;
+}
 
 
 
