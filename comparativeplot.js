47e6eeeee6474245b6fc20d58942d2e14e2cee57
adrianclim <aclim@sfu.ca>
2016-01-18 13:15:53 -0800

    add function to return genome with largest size on the plot, add g elements to Genome class

--- comparativeplot.js
+++ comparativeplot.js
@@ -3,24 +3,59 @@ function ComparativePlot(targetContainer){
     this.alignmentFile = null;
     this.container = d3.select(targetContainer);
     this.svg = null;
+    this.xscale = null;
+
+    this.createPlot();
 }
 
+ComparativePlot.prototype.createPlot = function(){
+    this.svg = this.container.append("svg")
+        .attr("class","comparativeplot");
+};
+
 ComparativePlot.prototype.addGenome = function(genome){
+    genomeContainer = this.svg.append("g")
+        .attr("class","genome");
+
+    genome.setContainer(genomeContainer);
     this.genomes.push(genome);
+};
+
+//TODO: Add functionality to remove genome from visualization
+ComparativePlot.prototype.removeGenome = function(indexGenome){
+    this.genomes.pop(indexGenome);
 };
 
 ComparativePlot.prototype.setAlignmentFile = function(alignmentfile){
     this.alignmentFile = alignmentfile;
 };
 
-ComparativePlot.prototype.render = function(){
-    this.svg = this.container.append("svg")
-        .attr("class","comparativeplot");
+ComparativePlot.prototype.findLargestGenome = function(){
+    var largestGenomeSize = -1;
+    var largestGenome = null;
+
+    for (var genomeindex = 0; genomeindex < this.genomes.length; genomeindex++){
+        if (this.genomes[genomeindex].getSize()>largestGenomeSize){
+            largestGenomeSize = this.genomes[genomeindex];
+            largestGenome = this.genomes[genomeindex];
+        }
+    }
+    return largestGenome;
 };
 
-function Genome(name){
+function Genome(name, size){
     this.genomeName = name;
+    this.genomeSize = size;
+    this.container = null;
 }
+
+Genome.prototype.setContainer = function(container){
+    this.container = container;
+};
+
+Genome.prototype.getSize = function(){
+    return this.genomeSize;
+};
 
 
 
